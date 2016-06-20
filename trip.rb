30f1e7cb81f1070540896fe514401f662edcb37d
Marti Gendel <martigendel@gmail.com>
2016-06-20 17:15:08 +0000

    Done.

---
+++ trip.rb
@@ -0,0 +1,6 @@
+puts "Where do you want to stay?"
+the_plaza = gets.chomp
+puts "How many nights do you want to stay?"
+four_nights= gets.chomp
+puts "What type of food are you intersted in eating?"
+italian_food= gets.chomp
